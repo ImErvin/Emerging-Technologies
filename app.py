9a2f24bfcf45dfffd9ce56b54ef60017ad531cb5
Ervin Mamutov <employervin@gmail.com>
2017-10-23 12:36:10 +0100

    Added a basic bootstrap 4 front end and served it using flask

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return app.send_static_file('index.html')
